a033adbb5e1a57401c9fa9012e23ed358677d024
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-06 11:03:26 -0500

    initial commit with basic sinatra skeleton

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source :rubygems
+
+# PostgreSQL driver
+gem 'pg'
+
+# Sinatra driver
+gem 'sinatra'
+
+# Use Thin for our web server
+gem 'thin'
+
+gem 'activesupport'
+gem 'activerecord'
+
+gem 'rake'
+gem 'bcrypt-ruby'
+
+gem 'shotgun'
+
+group :development, :test do
+  gem 'faker'
+  gem 'rspec'
+end
---
+++ app/models/user.rb
@@ -0,0 +1,25 @@
+class User < ActiveRecord::Base
+  attr_reader :entered_password
+
+  validates :entered_password, :length => { :minimum => 6 }
+  validates :email, :uniqueness => true, :format => /.+@.+\..+/ # imperfect, but okay
+
+  include BCrypt
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(pass)
+    @entered_password = pass
+    @password = Password.create(pass)
+    self.password_hash = @password
+  end
+
+  def self.authenticate(email, password)
+    user = User.find_by_email(email)
+    return user if user && (user.password == password)
+    nil # either invalid email or wrong password
+  end
+
+end
