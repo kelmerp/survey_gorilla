30fe79dcb8b6cf705c04080ce39c06a7a29350ba
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 15:32:10 -0500

    fixed response route

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -10,15 +10,19 @@ get '/survey/:id' do
   erb :take_survey
 end
 
+get '/survey/:survey_id/results' do
+  @survey_id = params[:survey_id]
+end
+
 # POST ==============================
 
 post '/survey/create' do
-  # p params
+  p params
   @survey = Survey.create(params[:survey])
-  @question = Question.create(:question => params[:question_1][:question])
-  @choice_one = Choice.create(:choice => params[:choice_1][:choice])
-  @choice_two = Choice.create(:choice => params[:choice_2][:choice])
-  @choice_three = Choice.create(:choice => params[:choice_3][:choice])
+  @question = Question.create(:question => params[:question][:question_1])
+  @choice_one = Choice.create(:choice => params[:choice][:choice_1])
+  @choice_two = Choice.create(:choice => params[:choice][:choice_2])
+  @choice_three = Choice.create(:choice => params[:choice][:choice_3])
   @survey.questions << @question
   @question.choices << @choice_one
   @question.choices << @choice_two
@@ -27,9 +31,10 @@ post '/survey/create' do
   redirect to "/survey/#{@survey.id}"
 end
 
-# post "/survey/submit" do
-#   p params
-#   @response = Response.create(:taker_id => current_user.id, :choice_id => params[:])
-   
-#   # redirect to "/survey/#{survey.id}/results"
-# end
+post "/survey/submit" do
+  p params
+  @response = Response.create(:taker_id => current_user.id, :choice_id => params[:question])
+  survey_id = params[:survey_id]
+  # "hello Nick"
+  redirect to "/survey/#{survey_id}/results"
+end
