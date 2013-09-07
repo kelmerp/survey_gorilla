a75a6ec03f758c80c111bf38b52575ea208ba07e
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-07 16:05:25 -0500

    it works

--- public/js/create_survey.js
+++ public/js/create_survey.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
     event.preventDefault();
   });
 
-  $('tbody' ).on('click', '.add_choice', function(){
+  $('tbody' ).on('click', '.add_choice', function(event){
     var last_choice = $(this).parent().parent().parent().find('tr').last();
     var next_id = parseInt($(last_choice).attr('id').split('choice_')[1])+1;
     
@@ -14,8 +14,24 @@ $(document).ready(function() {
     var new_choice = $(this).parent().parent().parent().find('tr').last();
     $(new_choice).find('td:nth-child(2)').find('input').attr("name", "choice_" + next_id);
     $(new_choice).find('td:nth-child(2)').find('input').attr("placeholder", "Enter Choice " + next_id);
+    event.preventDefault();
   });
 
+  $('form').on('click','.del_question',function(event){
+    $(this).parent().parent().parent().parent().remove();
+    event.preventDefault();
+  });
+
+  $('form').on('click', '.add_question', function(event){
+    var last_question = $(this).parent().parent().parent().parent().parent().find('table').last();
+    var next_id = parseInt($(last_question).attr('id').split('question_')[1])+1;
+    
+    $('.submit').prepend("<table id=question_" + next_id + ">" + last_question.html() + "</tr>");
+    // var new_question = $(this).parent().parent().parent().parent().parent().find('table').last();
+    // $(new_question).find('td:nth-child(2)').find('input').attr("name", "question_" + next_id);
+    // $(new_question).find('td:nth-child(2)').find('input').attr("placeholder", "Enter Question " + next_id);
+    event.preventDefault();
+  });
 
 
 });
