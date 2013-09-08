756221e1e5d8514f29df1f6ae21b134a83e375c2
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-08 15:33:29 -0500

    working on mult charts

--- public/js/create_chart.js
+++ public/js/create_chart.js
@@ -1,25 +1,42 @@
-$(document).ready(function() {
+var questions = $('.q_num');
 
-  //Get context with jQuery - using jQuery's .get() method.
-  var ctx = $("#myChart").get(0).getContext("2d");
-//This will get the first returned node in the jQuery collection.
-  var myNewChart = new Chart(ctx);
-  var data = [
-  {
-    value: 30,
-    color: "#F38630",
-    label: "Whatevs"
-  },
-  {
-    value: 50,
-    color: "#E0E4CC",
-    label: "Blargh"
-  },
-  {
-    value: 100,
-    color: "#69D2E7",
-    label: "Dat"
-  }     
-];
-  new Chart(ctx).Pie(data);
+$(document).ready(function() {
+  
+    var ctx = $("#myChart").get(1).getContext("2d");
+  $('.q_num').each(function(index) {
+    //Get context with jQuery - using jQuery's .get() method.
+    console.log(ctx);
+    //This will get the first returned node in the jQuery collection.
+    var myNewChart = new Chart(ctx);
+    
+    //One question at a time
+
+    //This is one question
+    var data = new Array();
+
+    //this is one option
+    data.push(
+    {
+      value: 30,
+      color: "#F38630",
+      label: "Whatevs"
+    });
+
+    data.push(
+    {
+      value: 50,
+      color: "#E0E4CC",
+      label: "Blargh"
+    });
+
+    data.push(
+    {
+      value: 100,
+      color: "#69D2E7",
+      label: "Dat"
+    });     
+
+    new Chart(ctx).Pie(data);
+  });
+
 });
